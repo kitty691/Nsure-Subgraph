9e9fbaf82d142b876ef9aea6eb2ddca899cb9136
Oleg Prilipko <prilipkoleg@gmail.com>
2020-12-10 23:27:32 +0200

    initial

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,83 +1,58 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
   Nsure,
-  Approval,
-  DelegateChanged,
-  DelegateVotesChanged,
-  OwnershipTransferred,
-  Transfer,
+  Transfer as TransferEvent,
+  Approval as ApprovalEvent,
+  DelegateChanged as DelegateChangedEvent,
+  DelegateVotesChanged as DelegateVotesChangedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
 } from "../generated/Nsure/Nsure"
-import { ExampleEntity } from "../generated/schema"
+import { Transfer, Approval, OwnershipTransferred } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = Approval.load(event.params.value.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
+    entity = new Approval(event.params.value.toHex())
     entity.count = BigInt.fromI32(0)
   }
 
-  // BigInt and BigDecimal math are supported
   entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
   entity.owner = event.params.owner
   entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
+  entity.value = event.params.value
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DELEGATION_TYPEHASH(...)
-  // - contract.DOMAIN_TYPEHASH(...)
-  // - contract.addMinter(...)
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.checkpoints(...)
-  // - contract.decimals(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.delMinter(...)
-  // - contract.getCurrentVotes(...)
-  // - contract.getMinterLength(...)
-  // - contract.getPriorVotes(...)
-  // - contract.increaseAllowance(...)
-  // - contract.isMinter(...)
-  // - contract.mint(...)
-  // - contract.name(...)
-  // - contract.nonces(...)
-  // - contract.numCheckpoints(...)
-  // - contract.owner(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
 }
 
 export function handleDelegateChanged(event: DelegateChanged): void {}
 
 export function handleDelegateVotesChanged(event: DelegateVotesChanged): void {}
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
+  let entity = OwnershipTransferred.load(event.params.previousOwner.toHex())
 
-export function handleTransfer(event: Transfer): void {}
+  if (entity == null) {
+    entity = new OwnershipTransferred(event.params.previousOwner.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = Transfer.load(event.params.value.toHex())
+
+  if (entity == null) {
+    entity = new Transfer(event.params.value.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+  entity.save()
+}
